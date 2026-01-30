da1eb7a4f2d9e56051e1d8a5c3abed2b80dd8526
sgagnon15 <sgagnon15@gmail.com>
2026-01-30 10:37:15 -0500

    Version 0.1

--- app/src/main/java/com/sergeapps/stock/ui/item/ItemsListScreen.kt
+++ app/src/main/java/com/sergeapps/stock/ui/item/ItemsListScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
